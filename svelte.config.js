29b8d1181be89122618a5b58f023cd1662c73dd6
Pinxian Lu <pl444@cornell.com>
2023-07-24 14:11:25 -0400

    vercel deployment bug fix attempt 2

--- svelte.config.js
+++ svelte.config.js
@@ -4,22 +4,27 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), preprocess({
-	  postcss: true
-	})],
-  
-	kit: {
-	  adapter: adapter({
-		runtime: 'nodejs18.x'
-	  }),
-	  vite: {
-		build: {
-		  rollupOptions: {
-			target: 'node',
-		  },
-		},
-	  },
-	}
-  };
-  
-  export default config;
+  preprocess: [vitePreprocess(), preprocess({
+    postcss: true
+  })],
+
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x'
+    }),
+  },
+
+  vite: {
+    ssr: {
+      target: 'node',
+    },
+    build: {
+      target: 'node',
+      rollupOptions: {
+        external: ['googleapis-common'],
+      },
+    },
+  },
+};
+
+export default config;
