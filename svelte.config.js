3fdfff169a0f9e510685a9a980f7d45cbd8bc406
Pinxian Lu <pl444@cornell.com>
2023-07-24 15:19:21 -0400

    vercel deployment bug fix attempt

--- svelte.config.js
+++ svelte.config.js
@@ -4,24 +4,45 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), preprocess({
-    postcss: true
-  })],
+  preprocess: [
+    // Set the target option to 'esnext'
+    vitePreprocess({ target: 'esnext' }),
+    preprocess({ postcss: true })
+  ],
 
   kit: {
     adapter: adapter({
       runtime: 'nodejs18.x'
-    }),
+    })
   },
 
-  vite: {
-    ssr: {
-      external: ['https'],
-    },
-    build: {
-      polyfillNode: true,
-    },
-  },
+  // Add the 'rollupOptions' here, not inside 'config.kit'
+  rollupOptions: {
+    // Add Node.js built-in modules to 'external'
+    external: ['stream', 'fs', 'https', 'url', 'util', 'http2', 'zlib', 'process', 'querystring', 'net', 'tls', 'assert', 'events', 'os', 'child_process', 'path', 'buffer', 'crypto'],
+  }
 };
 
 export default config;
+
+
+// import preprocess from "svelte-preprocess";
+// import adapter from '@sveltejs/adapter-vercel';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: [vitePreprocess(), preprocess({
+//         postcss: true
+//     })],
+
+// 	kit: {
+// 		adapter: adapter({
+//             runtime: 'nodejs18.x'
+//         })
+// 	}
+// };
+
+// export default config;
