2386f6fffb9a35e82f5ea7a5cd250659c5f0256f
Pinxian Lu <pl444@cornell.com>
2023-07-26 14:27:58 -0400

    bug fix

--- src/routes/api/spreadsheets/+server.ts
+++ src/routes/api/spreadsheets/+server.ts
@@ -1,6 +1,6 @@
-// import { google } from 'googleapis'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { google } from 'googleapis'
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
