c86f537f23469d1428900552644e21d94fd11947
Pinxian Lu <pl444@cornell.com>
2023-07-28 02:26:14 -0400

    bug fix

--- svelte.config.js
+++ svelte.config.js
@@ -23,23 +23,70 @@
 // export default config;
 
 
+// import preprocess from "svelte-preprocess";
+// import adapter from '@sveltejs/adapter-vercel';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: [vitePreprocess(), preprocess({
+//         postcss: true
+//     })],
+
+// 	kit: {
+// 		adapter: adapter({
+//             runtime: 'nodejs18.x'
+//         })
+// 	}
+// };
+
+// export default config;
+
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), preprocess({
-        postcss: true
-    })],
+  preprocess: [
+    vitePreprocess({ target: 'esnext' }),
+    preprocess({ postcss: true })
+  ],
 
-	kit: {
-		adapter: adapter({
-            runtime: 'nodejs18.x'
-        })
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x'
+    }),
+    vite: {
+      resolve: {
+        alias: {
+          fs: false,
+          path: false,
+          https: false,
+          url: false,
+          util: false,
+          http2: false,
+          zlib: false,
+          process: false,
+          querystring: false,
+          net: false,
+          tls: false,
+          assert: false,
+          events: false,
+          os: false,
+          child_process: false,
+          buffer: false,
+          crypto: false,
+        }
+      }
+    }
+  },
+
+  rollupOptions: {
+    external: ['stream', 'fs', 'https', 'url', 'util', 'http2', 'zlib', 'process', 'querystring', 'net', 'tls', 'assert', 'events', 'os', 'child_process', 'path', 'buffer', 'crypto'],
+  }
 };
 
 export default config;
