b44eaf5fd46782d22bba69f8178c756a114eef7e
Pinxian Lu <pl444@cornell.com>
2023-08-01 18:18:29 -0400

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -23,35 +23,23 @@
 // export default config;
 
 
-// import preprocess from "svelte-preprocess";
-// import adapter from '@sveltejs/adapter-vercel';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-// 	// for more information about preprocessors
-// 	preprocess: [vitePreprocess(), preprocess({
-//         postcss: true
-//     })],
-
-// 	kit: {
-// 		adapter: adapter({
-//             runtime: 'nodejs18.x'
-//         })
-// 	}
-// };
-
-// export default config;
-
 import preprocess from "svelte-preprocess";
-import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// Netlify
-import adapter from '@sveltejs/adapter-netlify';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), preprocess({
+        postcss: true
+    })],
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter({
+            runtime: 'nodejs18.x'
+        })
+	}
 };
+
+export default config;
