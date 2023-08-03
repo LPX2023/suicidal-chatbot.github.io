3ae771b2885e619ecff8dc255efe9b422200b378
Pinxian Lu <pl444@cornell.com>
2023-08-03 14:02:10 -0400

    adapter changes

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,19 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+declare namespace App {
+	interface Locals {}
+
+    interface Platform {
+       env: {
+           COUNTER: DurableObjectNamespace;
+       };
+       context: {
+           waitUntil(promise: Promise<any>): void;
+       };
+       caches: CacheStorage & { default: Cache }
+    }
+
+    interface Session {}
+
+    interface Stuff {}
 }
 
 export {}
